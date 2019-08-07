6c0766dbad00b43045e3bc3977780024cb87d658
SevenLink_Mac_Ale <alexandre.sevenlink@gmail.com>
2019-08-07 15:50:25 -0300

    vai

--- ADO Aula-05-08/teste_pib.py
+++ ADO Aula-05-08/teste_pib.py
@@ -1,6 +1,12 @@
 from ReadFile import ReadFile
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
 
 read = ReadFile()
 s = read.list_file('pib.txt')
-dic = read.pib_total(s)
-print(dic)
+d = read.list_file('regioes.txt')
+dic = read.to_dict(s)
+total = read.pib_total(s)
+pp.pprint(dic)
+# print(dic)
+
