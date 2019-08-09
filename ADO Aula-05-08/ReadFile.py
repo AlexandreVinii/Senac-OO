de29fc6532a3758bfe86cbdb4b14e334051b190d
Alexandre Vinicius <alexandreviniciusfs@gmail.com>
2019-08-08 23:03:35 -0300

    ADO_01 Finish

--- ADO Aula-05-08/ReadFile.py
+++ ADO Aula-05-08/ReadFile.py
@@ -19,7 +19,50 @@ class ReadFile:
         for i in range(len(pib)):
             aux += float(pib[i][1])
         return round(aux, 2)
+                
+    def sum(self, lista):
+        self.lista = lista
+        aux = 0
+        pib = lista
+        for i in range(len(pib)):
+            aux += float(pib[i][1])
+        return round(aux, 2)
 
+    def pib_states(self, lista):
+        self.lista = lista
+        read = ReadFile()
+        total = read.pib_total(self.lista)
+        for i in range(len(self.lista)):
+            aux = (float(lista[i][1])/total)*100
+            self.lista[i][1] = f'{aux:.2f}% - PIB'
+            
+        pib = read.to_dict(self.lista)
+        return pib
+
+    def sum_regioes(self, lista_reg, lista_pib):
+        self.lista_reg = lista_reg
+        self.lista_pib = lista_pib
+        read = ReadFile()
+        regioes = self.lista_reg
+        for i in range(len(self.lista_pib)):
+            for j in range(len(self.lista_reg)):
+                if self.lista_pib[i][0] == self.lista_reg[j][0]:
+                    aux = self.lista_pib[i][1]
+                    regioes[j].append(aux)
+        norte = read.sum(regioes[1:8])
+        nordeste = read.sum(regioes[10:19])
+        sudeste = read.sum(regioes[21:25])
+        sul = read.sum(regioes[27:30])
+        centro_oeste = read.sum(regioes[32:])
+        dicionario = {
+            'Norte' : norte,
+            'Nordeste' : nordeste,
+            'Sudeste' : sudeste,
+            'Sul' : sul,
+            'Centro-Oeste': centro_oeste
+        }
+        return dicionario
+        
     def to_dict(self, lista):
         self.lista = lista
         dic = {k:v for k,v in self.lista}
--- ADO Aula-05-08/teste_pib.py
+++ ADO Aula-05-08/teste_pib.py
@@ -2,11 +2,27 @@ from ReadFile import ReadFile
 import pprint
 pp = pprint.PrettyPrinter(indent=2)
 
+
+# #Instancia da Classe
 read = ReadFile()
-s = read.list_file('pib.txt')
-d = read.list_file('regioes.txt')
-dic = read.to_dict(s)
-total = read.pib_total(s)
-pp.pprint(dic)
-# print(dic)
+
+# #Método que lê um arquivo e retorna uma lista.
+pib_txt = read.list_file('pib.txt')
+regioes_txt = read.list_file('regioes.txt')
+#--------- # --------- # ---------- # ------------
+
+#Método que calcula o PIB TOTAL por Regioes
+soma_regioes = read.sum_regioes(regioes_txt,pib_txt)
+print('Somatória do PIB por Regiões')
+pp.pprint(soma_regioes)
+print('------------------------')
+#--------- # --------- # ---------- # ------------
+
+#Método que calcula porcentagem do PIB por estado
+print('Porcentagem do PIB por Estados')
+pib_states = read.pib_states(pib_txt)
+pp.pprint(pib_states)
+#--------- # --------- # ---------- # ------------
+
+
 
